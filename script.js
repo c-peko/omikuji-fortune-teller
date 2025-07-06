92d2acd1e04516f4e5a1e2f759afb61cb0775528
c-peko <87267447+c-peko@users.noreply.github.com>
2025-07-06 16:50:40 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const fortunes = [
-  "大吉 - Excellent luck!",
-  "中吉 - Good luck!",
-  "小吉 - Little luck!",
-  "吉 - Some luck!",
-  "末吉 - Slight luck.",
-  "凶 - Bad luck.",
+  "大吉 - Excellent luck!</br>Your wishes will come true!",
+  "中吉 - Good luck!</br>Keep working hard for your wishes to come true!",
+  "小吉 - Little luck!</br>Luck is on your side! Don't give up your wishes!",
+  "吉 - Some luck!</br>Don't hesitate to get help from others;)",
+  "末吉 - Slight luck.</br>Though it may take time, your dreams will come true - Patience is the key.",
+  "凶 - Bad luck.</br>Always have a plan B!",
   "大凶 - Very bad luck!"
 ];
 
 document.getElementById("drawBtn").addEventListener("click", () => {
   const randomIndex = Math.floor(Math.random() * fortunes.length);
   const fortuneText = fortunes[randomIndex];
-  document.getElementById("fortune").textContent = fortuneText;
+  document.getElementById("fortune").innerHTML = fortuneText;
 });
